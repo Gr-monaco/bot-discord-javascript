bde5558d15130df43121937de61b4974c7555dcb
Gabriel Monaco <gr-monaco@hotmail.com>
2022-02-04 15:23:08 -0300

    Adiciona mensagem de login

--- src/SalarioBot.js
+++ src/SalarioBot.js
@@ -4,4 +4,8 @@ require('dotenv').config();
 const { Client, Intents, Message } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
+client.on('ready', () => {
+    console.log(`${client.user.tag} está online!`);
+})
+
 client.login(process.env.BOT_TOKEN)
